3f86a46f5bcff3b4c2278534db93586029f3c632
KlaudiuszGL <106958085+KlaudiuszGL@users.noreply.github.com>
2022-06-09 13:15:12 +0200

    Create name.py

---
+++ name.py
@@ -0,0 +1,11 @@
+print("Give me your first and last name")
+name = input()
+
+plik = open("dane.json", 'w')
+plik.write('\n' + name)
+plik.close()
+
+## Dwa sposoby bo nie wiedziaem XD
+
+## with open('nowy.json', 'w') as f:
+## f.write("Klaudiusz Jaworski")
